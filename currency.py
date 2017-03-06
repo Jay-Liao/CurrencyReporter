aed13e3fc6fb70617a3c9df168163c4d258dc106
Jay-Liao <j99590314@gmail.com>
2017-03-06 16:00:07 +0800

    remove email info

--- currency.py
+++ currency.py
@@ -9,6 +9,8 @@ from retrying import retry
 import ConfigParser
 import os
 import smtplib
+import schedule
+import time
 
 
 @retry(stop_max_attempt_number=5, wait_fixed=1000)
@@ -51,30 +53,6 @@ def post_message_to_general(message):
         print 'post message to Slack fails.'
 
 
-# def post_message_to_statistic(message):
-#     web_hook_url = 'https://hooks.slack.com/services/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'
-#     response = requests.post(web_hook_url, json={"text": message})
-#     if response.status_code != 200:
-#         print 'post message to Slack fails.'
-
-
-# def is_time_in_valid_range(specific_time):
-#     start = datetime.time(9, 0, 0)
-#     end = datetime.time(15, 30, 0)
-#     is_time_in_valid_range_bool = (start <= specific_time.time() <= end)
-#     day_of_week = specific_time.strftime("%A") # Retrieve full name day of week from datetime ex. Monday
-#     is_time_in_work_day_bool = (day_of_week != 'Saturday' and day_of_week != 'Sunday')
-#     return is_time_in_valid_range_bool and is_time_in_work_day_bool
-
-
-# def post_statistic_report():
-#     report = ""
-#     for key, value in date_to_min_sell_spot_dict.items():
-#         report += '[' + date_to_time_info_dict.get(key) + ']: ' + str(value) + '\n'
-#     if report != "":
-#         post_message_to_statistic(report)
-
-
 def is_date_key_exist(date_key):
     config = ConfigParser.ConfigParser()
     config.read(MIN_SELL_SPOT_FILENAME)
@@ -95,11 +73,32 @@ def set_min_sell_spot_with_date_key(date_key, sell_spot):
     config.write(open(MIN_SELL_SPOT_FILENAME, 'wb'))
 
 
+def get_email():
+    config = ConfigParser.ConfigParser()
+    config.read('Config.ini')
+    email = config.get('notification', 'email')
+    return email
+
+
 def get_notify_price():
     config = ConfigParser.ConfigParser()
     config.read('Config.ini')
     notify_price = config.get('notify_price', 'notify_price')
     return float(notify_price)
+
+
+def get_reporter_email():
+    config = ConfigParser.ConfigParser()
+    config.read('Config.ini')
+    email = config.get('reporter', 'email')
+    return email
+
+
+def get_reporter_password():
+    config = ConfigParser.ConfigParser()
+    config.read('Config.ini')
+    password = config.get('reporter', 'password')
+    return password
 
 
 def append_new_line_to_file(message, filename):
@@ -118,37 +117,44 @@ def check_min_sell_spot_file():
         config.write(open(MIN_SELL_SPOT_FILENAME, 'wb'))
 
 
-def send_mail(to_address, message):
-    message = 'Subject: %s\n\n%s' % ('Currency Notification', message)
-    FROM_ADDRESS = "currency.reporter@gmail.com"
-    PASSWORD = "54323013"
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(FROM_ADDRESS, PASSWORD)
-    server.sendmail(FROM_ADDRESS, to_address, message)
-    server.quit()
+# def send_mail(to_address, the_message):
+#     content = 'Subject: %s\n\n%s' % ('Currency Notification', the_message)
+#     reporter_email = get_reporter_email()
+#     reporter_password = get_reporter_password()
+#     server = smtplib.SMTP('smtp.gmail.com', 587)
+#     server.starttls()
+#     server.login(reporter_email, reporter_password)
+#     server.sendmail(reporter_email, to_address, content)
+#     server.quit()
+
+
+def job():
+    check_min_sell_spot_file()
+    current_time = datetime.datetime.now()
+    current_date_string = str(current_time.strftime("%Y-%m-%d"))
+    sell_spot = get_sell_spot()
+    current_time_info = str(current_time.strftime("%Y-%m-%d(%a) %H:%M:%S"))
+    message = current_time_info + ': ' + str(sell_spot)
+    append_new_line_to_file(message, OUTPUT_FILENAME)
+    if not is_date_key_exist(current_date_string):
+        set_min_sell_spot_with_date_key(current_date_string, sell_spot)
+        if sell_spot < SELL_SPOT_NOTIFY_PRICE:
+            post_message_to_general(message)
+            # send_mail(EMAIL, 'USD: ' + str(sell_spot))
+    else:
+        min_sell_spot_today = get_min_sell_spot_by_date_key(current_date_string)
+        if sell_spot < min_sell_spot_today:
+            set_min_sell_spot_with_date_key(current_date_string, sell_spot)
+            if sell_spot < SELL_SPOT_NOTIFY_PRICE:
+                post_message_to_general(message)
+                # send_mail(EMAIL, 'USD: ' + str(sell_spot))
 
 
 SELL_SPOT_NOTIFY_PRICE = get_notify_price()  # 到價提醒
 OUTPUT_FILENAME = "output.txt"
 MIN_SELL_SPOT_FILENAME = "MinSellSpot.txt"
-check_min_sell_spot_file()
-current_time = datetime.datetime.now()
-current_date_string = str(current_time.strftime("%Y-%m-%d"))
-sell_spot = get_sell_spot()
-current_time_info = str(current_time.strftime("%Y-%m-%d(%a) %H:%M:%S"))
-message = current_time_info + ': ' + str(sell_spot)
-append_new_line_to_file(message, OUTPUT_FILENAME)
-# if is_time_in_valid_range(current_time):
-if not is_date_key_exist(current_date_string):
-    set_min_sell_spot_with_date_key(current_date_string, sell_spot)
-    if sell_spot < SELL_SPOT_NOTIFY_PRICE:
-        post_message_to_general(message)
-        send_mail('jay.liao@happygorgi.com', 'USD: ' + str(sell_spot))
-else:
-    min_sell_spot_today = get_min_sell_spot_by_date_key(current_date_string)
-    if sell_spot < min_sell_spot_today:
-        set_min_sell_spot_with_date_key(current_date_string, sell_spot)
-        if sell_spot < SELL_SPOT_NOTIFY_PRICE:
-            post_message_to_general(message)
-            send_mail('jay.liao@happygorgi.com', 'USD: ' + str(sell_spot))
+schedule.every().minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
